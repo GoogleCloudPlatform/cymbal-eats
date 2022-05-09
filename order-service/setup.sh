5d29f10664e4d4b018579d1138013d5f56479ea2
mmcgibbon <matthewmcgibbon@outlook.com>
2022-05-09 10:06:44 +0100

    npm install pubsub

--- order-service/setup.sh
+++ order-service/setup.sh
@@ -58,3 +58,5 @@ gcloud run deploy $ORDER_SERVICE_NAME \
   --project=$PROJECT_ID \
   --set-env-vars=INVENTORY_SERVICE_URL=$INVENTORY_SERVICE_URL \
   --quiet
+
+gcloud pubsub topics create orderTopic --project=$PROJECT_ID
