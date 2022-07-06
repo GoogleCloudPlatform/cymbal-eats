39c6c56be15560e5a5c3d3900fffca6f6ab2b690
Andrey Shakirov <andreyshakirov@google.com>
2022-07-06 14:48:55 -0400

    Renamed DURATION to FAILED_ITEM_AGE

--- cleanup-service/script.sh
+++ cleanup-service/script.sh
@@ -1,14 +1,14 @@
-echo "DURATION=$DURATION"
+echo "FAILED_ITEM_AGE=$FAILED_ITEM_AGE"
 echo "MENU_SERVICE_URL=$MENU_SERVICE_URL"
-# Failed items older than DURATION in minutes
-for id in $(http GET $MENU_SERVICE_URL/menu/failed | jq '[.[] | select(.updateDateTime < ((now - 60 * (env.DURATION | tonumber) )| strftime("%Y-%m-%dT%H:%M:%S.%f")))]'| jq '.[].id'); do
+# Failed items older than FAILED_ITEM_AGE in minutes
+for id in $(http GET $MENU_SERVICE_URL/menu/failed | jq '[.[] | select(.updateDateTime < ((now - 60 * (env.FAILED_ITEM_AGE | tonumber) )| strftime("%Y-%m-%dT%H:%M:%S.%f")))]'| jq '.[].id'); do
    echo "Deleting Menu Item : $MENU_SERVICE_URL/menu/$id"
    http GET $MENU_SERVICE_URL/menu/$id
    http DELETE $MENU_SERVICE_URL/menu/$id
 done
 
-# Processing items older than DURATION in minutes
-for id in $(http GET $MENU_SERVICE_URL/menu/processing | jq '[.[] | select(.updateDateTime < ((now - 60 * (env.DURATION | tonumber))| strftime("%Y-%m-%dT%H:%M:%S.%f")))]'| jq '.[].id'); do
+# Processing items older than FAILED_ITEM_AGE in minutes
+for id in $(http GET $MENU_SERVICE_URL/menu/processing | jq '[.[] | select(.updateDateTime < ((now - 60 * (env.FAILED_ITEM_AGE | tonumber))| strftime("%Y-%m-%dT%H:%M:%S.%f")))]'| jq '.[].id'); do
  echo "Deleting Menu Item : $MENU_SERVICE_URL/menu/$id"
  http GET $MENU_SERVICE_URL/menu/$id
  http DELETE $MENU_SERVICE_URL/menu/$id
