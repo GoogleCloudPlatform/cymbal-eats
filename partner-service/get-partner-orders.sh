284156b6261d97d7be1f12c85152fe427b3bb05e
momander <momander@google.com>
2022-08-26 16:49:34 -0700

    Remove old partner-service

--- partner-service/get-partner-orders.sh
+++
@@ -1,1 +0,0 @@
-curl https://partner-service-luu7kai33a-uc.a.run.app/partner-order/9ewnv58tuy4
--- partner-service/post-example-order.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d "@example-order.json" \
-  https://partner-service-luu7kai33a-uc.a.run.app/partner-order
